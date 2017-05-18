c52dab50c3c0d8c687d66bc4e61208ccd9d620d7
nguyenphuckhanh91 <nguyenphuckhanh91@gmail.com>
2017-05-18 17:04:33 +0700

    Add Pause, Play Image. Display Video Player Controls

--- YoutubeApp/VideoLauncher.swift
+++ YoutubeApp/VideoLauncher.swift
@@ -11,19 +11,93 @@ import AVFoundation
 
 class VideoPlayerView: UIView {
     
+    let activityIndicatorView: UIActivityIndicatorView = {
+        let aiv = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        aiv.translatesAutoresizingMaskIntoConstraints = false
+        aiv.startAnimating()
+        return aiv
+    }()
+    
+    lazy var pausePlayButton: UIButton = {
+        let button = UIButton(type: .system)
+        let image = UIImage(named: "pause")
+        button.setImage(image, for: UIControlState())
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = .white
+        button.isHidden = true
+        
+        button.addTarget(self, action: #selector(handlePause), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    var isPlaying = false
+    
+    func handlePause() {
+        if isPlaying {
+            player?.pause()
+            pausePlayButton.setImage(UIImage(named: "play"), for: UIControlState())
+        } else {
+            player?.play()
+            pausePlayButton.setImage(UIImage(named: "pause"), for: UIControlState())
+        }
+        
+        isPlaying = !isPlaying
+    }
+    
+    let controlsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 1)
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = UIColor.black
         
-        let urlString = "https://firebasestorage.googleapis.com/v0/b/youtubeclone-476c2.appspot.com/o/New%20Divide%20-%20Linkin%20Park%20%5BFingerstyle%20Guitar%20Cover%20by%20Eddie%20van%20der%20Meer%5D.mp4?alt=media&token=ab9511b4-b81d-474d-a7f4-fc591044ac82"
-        if let url = NSURL(string: urlString) {
-            let player = AVPlayer(url: url as URL)
-            
+        setupPlayerView()
+        
+        controlsContainerView.frame = frame
+        addSubview(controlsContainerView)
+        
+        controlsContainerView.addSubview(activityIndicatorView)
+        activityIndicatorView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        activityIndicatorView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        controlsContainerView.addSubview(pausePlayButton)
+        pausePlayButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        pausePlayButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        pausePlayButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        pausePlayButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        backgroundColor = .black
+    }
+    
+    var player: AVPlayer?
+    
+    fileprivate func setupPlayerView() {
+        //warning: use your own video url here, the bandwidth for google firebase storage will run out as more and more people use this file
+        let urlString = "https://firebasestorage.googleapis.com/v0/b/gameofchats-762ca.appspot.com/o/message_movies%2F12323439-9729-4941-BA07-2BAE970967C7.mov?alt=media&token=3e37a093-3bc8-410f-84d3-38332af9c726"
+        if let url = URL(string: urlString) {
+            player = AVPlayer(url: url)
             
             let playerLayer = AVPlayerLayer(player: player)
             self.layer.addSublayer(playerLayer)
             playerLayer.frame = self.frame
-            player.play()
+            
+            player?.play()
+            
+            player?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
+        }
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        //this is when the player is ready and rendering frames
+        if keyPath == "currentItem.loadedTimeRanges" {
+            activityIndicatorView.stopAnimating()
+            controlsContainerView.backgroundColor = .clear
+            pausePlayButton.isHidden = false
+            isPlaying = true
         }
     }
     
@@ -34,16 +108,16 @@ class VideoPlayerView: UIView {
 
 class VideoLauncher: NSObject {
     
-    
     func showVideoPlayer() {
-        print("Showing video player animation...")
+        print("Showing video player animation....")
         
         if let keyWindow = UIApplication.shared.keyWindow {
             let view = UIView(frame: keyWindow.frame)
             view.backgroundColor = UIColor.white
             
             view.frame = CGRect(x: keyWindow.frame.width - 10, y: keyWindow.frame.height - 10, width: 10, height: 10)
-            // 16x9 is the aspect ratio of all HD videos.
+            
+            //16 x 9 is the aspect ratio of all HD videos
             let height = keyWindow.frame.width * 9 / 16
             let videoPlayerFrame = CGRect(x: 0, y: 0, width: keyWindow.frame.width, height: height)
             let videoPlayerView = VideoPlayerView(frame: videoPlayerFrame)
@@ -51,12 +125,13 @@ class VideoLauncher: NSObject {
             
             keyWindow.addSubview(view)
             
-            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: { 
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                
                 view.frame = keyWindow.frame
+                
             }, completion: { (completedAnimation) in
-                //
+                //maybe we'll do something here later...
                 UIApplication.shared.setStatusBarHidden(true, with: .fade)
-            
             })
         }
     }
